9b29a28cd7fcf401795b6c9d7f6484175eae0329
Vitor Domingos <vm486092@gmail.com>
2024-01-19 18:55:56 -0200

    pasta src enviada

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let precoCombustivel = 5.56;
+let kmPorLitros = 10;
+let distanciaDaViagemEmKmParaViagemParaSalvador = 1920; 
+
+let litrosConsumidos = distanciaDaViagemEmKmParaViagemParaSalvador / kmPorLitros
+
+let valorGasto = litrosConsumidos * precoCombustivel
+
+
+console.log(valorGasto)
